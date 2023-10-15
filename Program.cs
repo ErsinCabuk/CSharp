dfa3b4a38b166056108dae1ca5327f882c0d5b49
Ersin <113672117+ErsinCabuk@users.noreply.github.com>
2023-10-15 15:29:53 +0300

    created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!")
+        }
+    }
+}
